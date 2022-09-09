2f7470a902385268818c84eeddd9edd3e0c4f60e
Peer Hoffmann <peerhoffmann81@gmail.com>
2022-09-09 09:10:20 +0200

    final argpars ex

--- Exersises/PythonnCodeing Ex_Arguments_01/PythonCodeing Ex_Arguments_02.py
+++ Exersises/PythonnCodeing Ex_Arguments_01/PythonCodeing Ex_Arguments_02.py
@@ -12,8 +12,10 @@ os.system("clear")
 
 parser = argparse.ArgumentParser(description="Arguments")
 
+# -- fast argument
 parser.add_argument("--fast", help="fast mode enabled", action="store_true")
 
+# firstname argument
 parser.add_argument(
     "first_name",
     type=str,
@@ -22,6 +24,8 @@ parser.add_argument(
     help="Enter your first name",
     nargs="?",
 )
+
+# lastname argument
 parser.add_argument(
     "last_name",
     type=str,
@@ -30,15 +34,20 @@ parser.add_argument(
     help="Enter your last name",
     nargs="?",
 )
+# age argument
 parser.add_argument(
     "age", type=int, metavar="[AGE]", default=100, help="Enter your age", nargs="?"
 )
 
+# pass all arguments into args
 args = parser.parse_args()
 
+#print result
 print(
     f"Hello {args.first_name} {args.last_name}!"
     f"\nI see that you have had {args.age+1} birthdays."
 )
+
+# print if fast-mode is enabled
 if args.fast is True:
     print("Hell yeah and you are on your fast mode !!!")
