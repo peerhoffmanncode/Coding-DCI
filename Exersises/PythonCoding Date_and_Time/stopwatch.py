bb0f056c53e44f0a8f720d93127558ff35f5c9bb
Peer Hoffmann <peerhoffmann81@gmail.com>
2022-09-14 19:02:44 +0200

    update

--- Exersises/PythonCoding Date_and_Time/stopwatch.py
+++ Exersises/PythonCoding Date_and_Time/stopwatch.py
@@ -3,17 +3,19 @@ import os
 
 os.system("clear")
 
-current_time = time.time()
+current_time = time.time() # start time / timestamp / current time
 
 while True:
     try:
         print(f"Stopwatch: {round(time.time() - current_time, 1)} seconds passed...       ", 
               end = "\r", flush = True)
         time.sleep(0.1)
+        
     except KeyboardInterrupt:
         print(f"Stopwatch: {round(time.time() - current_time, 1)} seconds DONE!!!         ", 
               end = "\r")
         break
+
     
 print("")
 print("\n")
