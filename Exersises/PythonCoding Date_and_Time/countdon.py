48727f90db8751edb367e7fc08319f593f5677fe
Peer Hoffmann <peerhoffmann81@gmail.com>
2022-09-21 10:23:01 +0200

    update time

--- Exersises/PythonCoding Date_and_Time/countdon.py
+++ Exersises/PythonCoding Date_and_Time/countdon.py
@@ -3,9 +3,9 @@ import os
 
 os.system("clear")
 
-store_current_time = time.time()
-
 n = int(input("Give a number to count down to : "))
+
+store_current_time = time.time()
 for i in range(n, 0, -1):
     print (f"countdown: {i}...", end="\r", flush= True)
     time.sleep(1)
