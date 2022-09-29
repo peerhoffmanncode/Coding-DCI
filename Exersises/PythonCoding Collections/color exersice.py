ada458729076115dde4e5d48fab597e1b013dbec
Peer Hoffmann <peerhoffmann81@gmail.com>
2022-09-29 11:48:37 +0200

    update

---
+++ Exersises/PythonCoding Collections/color exersice.py
@@ -0,0 +1,12 @@
+colors = ['red', 'green', 'yellow']
+
+# for loop
+string = ""
+for e in colors:
+    print(f"I love {e}")
+
+# while loop
+counter = 0
+while counter <= len(colors) -1:
+    print(f"I love {colors[counter]}")
+    counter += 1
