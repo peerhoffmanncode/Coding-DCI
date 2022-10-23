8d54b383c84ee1eaa49bc181419c2604040bd0e8
PeerHoffmannCode <peerhoffmann81@gmail.com>
2022-10-23 02:19:55 +0200

    docker

--- Class_Training/web-app/app.py
+++ Class_Training/web-app/app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<strong>Hello, World!</strong>"
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=5000)
