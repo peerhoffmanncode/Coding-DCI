7f320572a1c01d05def3525237b3311c4fc4ad44
PeerHoffmannCode <peerhoffmann81@gmail.com>
2022-10-26 23:36:47 +0200

    stuff

--- Class_Training/Classes/26-okt/Other Example.py
+++ Class_Training/Classes/26-okt/Other Example.py
@@ -1,6 +1,7 @@
 # code
 # A Python program to demonstrate inheritance
 
+
 class Person:
 
     # Constructor
@@ -14,16 +15,15 @@ class Person:
 
 
 class Emp(Person):
-
     def __init__(self, name, id):
         super().__init__(name, id)
         self.name_ = name
 
     def Print(self):
-        return("Emp class called")
+        return "Emp class called"
 
 
 Emp_details = Emp("2Mayank", 103)
 
 # calling parent class function
-print("2",Emp_details.name, Emp_details.Print())
+print("2", Emp_details.name, Emp_details.Print())
--- Class_Training/Classes/26-okt/student.py
+++ Class_Training/Classes/26-okt/student.py
@@ -1,8 +1,10 @@
 from person import Person
 
+
 class Student(Person):
     # no constructor
     pass
+
 
 class StudentClimber(Person):
     def __init__(self, first_name, last_name, gender, legs, third_eye):
@@ -10,12 +12,17 @@ class StudentClimber(Person):
         # super().__init__(first_name, last_name, gender, legs)
         self.third_eye = third_eye
 
+    def run(self, destination, origin):
+        print("---> SOMETHING!")
+        return super().run(destination, origin)
+
     def climb(self):
         return f"I, {self.first_name}, prefer to climb Mt. Everest"
 
-if __name__ == '__main__':
-    s1 = Student('Peer', 'Doe', 'Other')
+
+if __name__ == "__main__":
+    s1 = Student("Peer", "Doe", "Other")
     s2 = StudentClimber("Shaban", "Doe", "Male", 2, True)
     # print(s1.run('Berlin', 'Hamburg'))
     # print(s1)
-    print(s2.climb())
+    print(s2.run("A", "B"))
