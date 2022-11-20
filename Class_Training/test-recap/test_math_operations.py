23838efea6f5b6827f2366ef3b71468799e9f2e2
PeerHoffmannCode <peerhoffmann81@gmail.com>
2022-11-20 13:14:34 +0100

    rearragen

--- Class_Training/test-recap/test_math_operations.py
+++ Class_Training/test-recap/test_math_operations.py
@@ -8,7 +8,7 @@ from math_operations import add, sub
 class MathTest(unittest.TestCase):
     def test_add(self):
         result = add(2, 2)
-        expected_result = 5
+        expected_result = 4
         # assertions
         self.assertEqual(result, expected_result, "This does not work properly")
 
